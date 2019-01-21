95435b774f49bf4da934ac83aad195f0de7a244a
finepix <499768985@qq.com>
2019-01-21 14:43:38 +0800

    添加检测设备是否在线的判断

--- Selenium_learn/net_validate.py
+++ Selenium_learn/net_validate.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import time
+import os
 
 # initialization
 browser = webdriver.Chrome('chrome_driver/chromedriver.exe')
@@ -9,6 +10,10 @@ pwd = '0016247430'
 
 
 def login():
+    """
+            校园上网验证
+    :return:
+    """
     browser.get(url)
     login_btns = browser.find_element_by_id('mode_password')\
         .find_element_by_css_selector('.login_body > .btn_ok')\
@@ -35,6 +40,32 @@ def login():
     submit_btn = browser.find_element_by_id('password_submitBtn')
     submit_btn.click()
 
+    # 关闭浏览器
+    time.sleep(1)
+    browser.close()
+
+
+def is_online():
+    """
+                判断当前是否可连接网络
+    :return:
+    """
+    test_domain = 'www.baidu.com'
+    cmd = 'ping ' + test_domain
+    exit_code = os.system(cmd)
+    if exit_code:
+        return False
+    else:
+        return True
+
 
 if __name__ == '__main__':
-    login()
+
+    while True:
+        if is_online():
+            print('设备已联网，等待20分钟重新检测')
+            time.sleep(1200)
+        else:
+            print('设备不在线，启动注册程序！')
+            login()
+            print('注册成功，已联网。')
